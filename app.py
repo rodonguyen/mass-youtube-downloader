7370dc660b7341d7a476a1e88c262c39ede06a8d
Rodo <rodonguyendd@gmail.com>
2022-10-13 02:24:32 +1000

    Re-organise UI

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-import tkinter, os
+import tkinter, os, threading
+import subprocess
+from turtle import down
+
+from numpy import full
 
 window = tkinter.Tk()
-window.geometry("1000x1000")
+window.geometry("750x750")
 window.title("Mass YouTube Downloader") 
 
 font = ("Courier", 12, "bold")
-download_formats = { "MP3" : "mp3",
-                    "MP4" : "mp4", }
+download_formats = {  "Audio" : "audio",
+                      "Video" : "video", }
 
 
 url_var=tkinter.StringVar()
@@ -17,21 +21,38 @@ def download():
   url = url_var.get()
   format = download_format_var.get()
   
-  if format == 'mp3':
-    format = '--audio-format' + format 
-  os.system(f"youtube-dl -x {format} --retries 10 --ignore-errors  {url}")
+  if format == 'audio':
+    format = "-x --audio-format mp3"
+  if format == 'video':
+    format = "-f 'mp4[filesize<10M]+bestaudio/bestvideo[filesize<50M]+bestaudio'" 
+
+  full_command = f"youtube-dl {format} --retries 10 --ignore-errors {url}"
+
+  print(full_command)
+  os.system(full_command)
+  # print('===> ' + subprocess.check_output(full_command.split(' ')))
+
+def run_background(afunction, args=()):
+  thread = threading.Thread(target=afunction, args=args)
+  thread.start()
 
 
+# Entry
 url_input = tkinter.Entry(window, width=50, textvariable = url_var, font=font)
-url_input.insert(0, 'https://www.youtube.com/watch?v=zaBy3X37Oa8')
-url_input.pack(side='top', ipady=5)
+url_input.insert(0, 'https://youtu.be/FIhonBl-_pM')
+url_input.grid(row=0,column=0)
+row_count=1
+
+# Radio buttons  x 2
 for (text, value) in download_formats.items():
-  tkinter.Radiobutton(window, text = text, font=font, variable = download_format_var, value = value).pack(side='top', ipady=5)
+  tkinter.Radiobutton(window, text = text, font=font, variable = download_format_var, value = value).grid(row=row_count,column=0)
+  row_count += 1
+
+# Button
+download_button = tkinter.Button(window, text='Download', font=font, command = lambda : run_background(download)).grid(row=3,column=0)
 
 
-download_button = tkinter.Button(window, text='Download', font=font, command = download).pack(side='top', ipady=5)
 
-
-# sample_text.configure(font = font)
 
 window.mainloop()
+# https://www.geeksforgeeks.org/python-tkinter-tutorial/#basic
